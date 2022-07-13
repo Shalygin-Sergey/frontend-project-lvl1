d2c7b4160edcc521e146ae8cbc10620f779e65b9
serjjs <shs@trueflip.io>
2022-07-13 19:01:40 +0300

    added files and dependences

---
+++ bin/brain-games.js
@@ -0,0 +1,5 @@
+#!/bin/env node
+
+import greetAndAskForName from '../src/cli.js';
+
+greetAndAskForName();
---
+++ src/cli.js
@@ -0,0 +1,13 @@
+import readlineSync from 'readline-sync';
+
+const greetAndAskForName = () => {
+    console.log('Welcome to the Brain Games!');
+    
+    const name = readlineSync.question('May I have your name? ');
+  
+    console.log(`Hello, ${name}!`);
+  
+    return name;
+  };
+  
+  export default greetAndAskForName;
